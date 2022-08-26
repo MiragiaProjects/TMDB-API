b6198785a8b3209c902a4c2c42f697501592b994
Malin Olsson <miragia.solinde@gmail.com>
2022-08-26 14:34:53 +0200

    Added content to ToplistedMoviespage
    Added content to ToplistedMoviesPage

--- src/pages/ToplistedMoviesPage.jsx
+++ src/pages/ToplistedMoviesPage.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container'
+import Alert from 'react-bootstrap/Alert'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import { useQuery } from 'react-query'
+import MovieCard from '../components/MovieCard'
+import TMDBAPI from '../services/TMDB'
 
 const ToplistedMoviesPage = () => {
+    const { isLoading, isError, error, data} = useQuery('movie', TMDBAPI.getToplistedMovies)
   return (
-    <div>ToplistedMoviesPage</div>
+    <Container>
+    <h2>Toplisted Movies</h2>
+
+    {isLoading && (<p className='my-3'>Loading...</p>)}
+
+    {isError && ( <Alert variant="danger">
+        <h4>Ops, error!</h4>
+        <p>{error.message}</p>
+    </Alert>
+    )}
+
+    {data && (
+      <>
+      <Row>
+
+        {data.map( movie => (
+          <Col lg={3} md={4} sm={6} key={movie.id}>
+            <MovieCard movie={movie}/>
+          </Col>
+          
+        ))}
+      </Row>
+      </>
+    )}
+
+    </Container>
   )
 }
 
