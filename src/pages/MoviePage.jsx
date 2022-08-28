b6e66c2f10f93b7fad9c0933ef9b9fae57b9a9f4
Malin Olsson <miragia.solinde@gmail.com>
2022-08-28 15:07:42 +0200

    Forgot to save
    Forgot to save MovePage

--- src/pages/MoviePage.jsx
+++ src/pages/MoviePage.jsx
@@ -5,7 +5,7 @@ import { getMovie} from '../services/TMDB'
 import Container  from 'react-bootstrap/Container'
 import Alert  from 'react-bootstrap/Alert'
 import Image from 'react-bootstrap/Image'
-import useMovie from '../hooks/useMovie'
+
 
 
 const MoviePage = () => {
@@ -26,32 +26,40 @@ const MoviePage = () => {
 
         {data &&
         <>
-        <div className=''>
-            <h1>{data.title}</h1>
             <div>
-                {data && data.genres.map(genre =>
-                    <span className='me-2' key={genre.id}>
-                        <Link to={`/genres/${genre.id}`}>
-                            {genre.name}
-                        </Link>
-                    </span>)}
+        
+                <h3>{data.title}</h3>
+
+                {data.poster_path &&(
+                    <img src={`https://image.tmdb.org/t/p/w500${data.poster_path}`} />
+                )}
+                <div className='d-flex flex-column'>
+                    <p>Genre:{data.genres.map(genre => genre.name)}</p>
+                    <p>Released:{data.release_date}</p>
+                    <p>Rating: {data.vote_average}</p>
+                    <h3>Overview</h3>
+                    <p>{data.overview}</p>
+                </div>
             </div>
-        </div>
-        <div>
-            <Image className='img-fluid me-3' src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}/>
 
             <div>
-                <p>Released in:{data.release_date}</p>
-                <p>Runtime:{data.runtime} </p>
+                <h3>Cast</h3>
+                <div className='d-flex'>
+                    {data.credits.cast.map(cast => (
+                        <Card key={cast.id} className='w-20'>
+                            {cast.profile_path && (
+                                <Card.Img variant='top' src={`https://image.tmdb.org/t/p/w500${cast.profile_path}`} />
+                            )}
+                            <Card.Body className='d-flex flex-column'>
+                                <Card.Title>{cast.name}</Card.Title>
+                                <Card.Text>{cast.character}</Card.Text>
+                                <Button as={Link} to={`/actor/${cast.id}`} variant="primary">Read more ....</Button>
+                            </Card.Body>
+                        </Card>
+                    ))}
+                </div>
             </div>
-        </div>
 
-        <h3>Actors:</h3>
-        {data.credits.cast.map(cast => (
-            <div key={cast.id}>
-                <CastCard cast={cast} />
-            </div>
-        ))}
         </>
         }
     </Container>
